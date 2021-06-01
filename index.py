663e821c86853515f720bd643c760190defe2b00
wasiq abdullah <chwasiq0569>
2021-06-01 07:21:38 -0700

    Updated Correct Values

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ sheet = wb['Sheet1']
 # cell = sheet.cell(1, 1)
 for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
-    corrected_Value = cell.value * 0.9
+    corrected_Value = cell.value * 0.8
     corrected_Value_cell = sheet.cell(row, 4)
     corrected_Value_cell.value = corrected_Value
     print(sheet.cell(row, 4).value)
